28c1b15e508fcd5dc888db2e6f2b80c6c0cf6fd5
Password=1 <student@ws525-527-12.edu.local>
2024-11-16 16:30:30 +0300

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  a = 'Lama';
+  letters = ['George','Vova','Vika','Sam','Sammy','Samantha','Marlin','Riba','tunec`','Vova'];
   constructor() {}
 
 }
